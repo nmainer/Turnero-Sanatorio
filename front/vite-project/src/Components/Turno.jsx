0c218900ee5c297bd34f46a632c5cbf9bb3e4e14
nmainer <mainermnicolas@gmail.com>
2024-06-21 17:30:51 -0300

    correciones finales React Lifecycle

--- front/vite-project/src/Components/Turno.jsx
+++ front/vite-project/src/Components/Turno.jsx
@@ -2,7 +2,9 @@ import styles from "../CSSComponent/Turno.module.css"
 
 const Turno = (props)=>{
    const {dia,hora,doctor,estado} = props;
-  
+
+  const estadoEpalabra = estado === 'cancelado' ? styles.cancelado : styles.pendiente ;
+  const mostrarBoton =  estado ==='pendiente' ; 
 
 return (
 
@@ -10,8 +12,9 @@ return (
 <h1 className={styles.linea}>{dia}</h1>
 <h1 className={styles.linea}>{hora}</h1>
 <h1 className={styles.linea}>{doctor}</h1>
-<h1 className={`${estado === 'cancelado' ? styles.cancelado : styles.pendiente}`}>{estado}</h1>
-<button className={styles.linea}>Cancelar turno</button>
+<h1 className={estadoEpalabra}>{estado}</h1>
+{mostrarBoton && <button>Cancelar turno</button>}
+
 </div>
 )
 
