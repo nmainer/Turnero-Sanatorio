7960860847bb2d6e2b80784393bb2bdbc88cfc8d
nmainer <mainermnicolas@gmail.com>
2024-11-16 20:42:34 -0300

    cambios

---
+++ back/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.get('/api', (req, res) => {
+  res.json({ message: 'API funcionando!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- front/vite-project/vite.config.js
+++ front/vite-project/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   server: {
-    mimeTypes: {
-      'jsx': 'text/javascript'
+    proxy: {
+      '/api': 'http://localhost:3000', 
     },
-    hmr: true,
   },
 });
+
 
