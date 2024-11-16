7ee4e2f5c3770b3deeeb32f9f361756e18ae1d8e
nmainer <mainermnicolas@gmail.com>
2024-11-16 03:18:01 -0300

    cambios

--- back/api/index.ts
+++ back/api/index.ts
@@ -2,12 +2,12 @@ import app from '../src/server';  // Asegúrate de que app está exportado desde
 import "reflect-metadata";  // Necesario para TypeORM
 import { AppDataSource } from "../src/Config/data-source";
 import { VercelRequest, VercelResponse } from '@vercel/node';  // Importa las interfaces de Vercel
-import dotenv from 'dotenv';  // Para manejar las variables de entorno
 
-// Carga las variables de entorno
+// Cargar las variables de entorno
+import dotenv from 'dotenv';
 dotenv.config();
 
-// Inicializa la base de datos
+// Conectar a la base de datos
 let isConnected = false;
 
 AppDataSource.initialize()
@@ -19,24 +19,10 @@ AppDataSource.initialize()
     console.error('Error de conexión:', error);
   });
 
-// Obtiene el puerto desde las variables de entorno o usa 3000 como valor por defecto
-const PORT = process.env.PORT || 3000;
-
-// Exporta la aplicación como una función para que Vercel la ejecute
+// Exporta la función para que Vercel la ejecute
 export default (req: VercelRequest, res: VercelResponse) => {
-  // Verifica si la base de datos ya está conectada, si no, intenta conectar
   if (!isConnected) {
     return res.status(500).json({ message: 'Base de datos no conectada' });
   }
-
-  // Llama al servidor Express para manejar la solicitud
-  app(req, res);  // Pasa la solicitud y respuesta a tu aplicación Express
+  app(req, res);  // Pasar la petición y respuesta a tu app de Express
 };
-
-
-
-
-
-
-
-
