05c549e833ca89e1a2e4cdfa550bacc461f0b87a
nmainer <mainermnicolas@gmail.com>
2024-11-17 16:16:46 -0300

    cambios

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+
 
 export default defineConfig({
-  build: {
-    outDir: 'vite-project/dist',  // Define que la salida de la compilación será en "vite-project/dist"
-    emptyOutDir: true,  // Limpia la carpeta antes de cada compilación
-  },
-  server: {
-    port: 3000,  // O el puerto que desees
-  },
+ plugins:[react()]
 });
 
 
