65323bb46b9b2d798b6bfca721dc908749992bf2
nmainer <mainermnicolas@gmail.com>
2024-11-17 15:09:36 -0300

    cambios

--- front/vite-project/vite.config.js
+++ front/vite-project/vite.config.js
@@ -1,16 +1,12 @@
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist',  // Aquí defines que la salida de la compilación se guardará en la carpeta "dist"
-    emptyOutDir: true,  // Esto asegura que la carpeta "dist" se limpie antes de cada build
-  },
-  server: {
-    // Aquí puedes configurar el puerto o las opciones del servidor para desarrollo
-    port: 3000,
+    outDir: 'dist', // Este es el directorio de salida esperado
+    emptyOutDir: true,
   },
 });
+
 
 
 
