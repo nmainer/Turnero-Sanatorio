1c9af21c0783bbbac6d30a6d173eea349b1b7d46
nmainer <mainermnicolas@gmail.com>
2024-11-17 11:09:36 -0300

    cambios

--- front/vite-project/vite.config.js
+++ front/vite-project/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Aquí defines que la salida de la compilación se guardará en la carpeta "dist"
+    emptyOutDir: true,  // Esto asegura que la carpeta "dist" se limpie antes de cada build
+  },
   server: {
-    proxy: {
-      '/api': 'http://localhost:3000', 
-    },
+    // Aquí puedes configurar el puerto o las opciones del servidor para desarrollo
+    port: 3000,
   },
 });
+
 
 
